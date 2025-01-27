f1138e9c9780706b9560f85bfbbf0a7d822a1d85
Seyed Amirreza Mousavi Alamdardehi <84737203+seyedalamdar@users.noreply.github.com>
2025-01-27 14:27:49 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// index.js
-console.log("Hello from Choreo Example Component!");
+// src/index.js
+const http = require("http");
+
+const PORT = process.env.PORT || 8080; // Ensure it matches the port in component.yaml
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, Choreo!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
